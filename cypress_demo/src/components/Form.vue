c545efbeed62b7b7ed1b07ebd243a0bef3371c83
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2018-10-21 13:13:06 +0200

    legg til demoprosjekt

---
+++ cypress_demo/src/components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <form id='form'>
+    <input type="text"  v-model="myname">
+    <label >Name</label>
+    <input type="submit" @click="handleSubmit">
+    <span>{{ mystatus }}</span>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Form extends Vue {
+  private myname:string = ''
+  private mystatus:string = ''
+
+  handleSubmit() {
+    if(!!this.myname) {
+      this.mystatus = `${this.myname} Er registert`;
+    } else {
+      this.mystatus = 'Navn må fylles ut';
+    }
+  }
+  data() {
+    return {
+      myname: this.myname,
+      status: this.mystatus,
+    };
+  }
+}
+</script>
---
+++ cypress_demo/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Jeg har laget denne for å demonstrere bruk av Cypress."/>
+    <Form />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Form from '@/components/Form.vue';
+
+@Component({
+  components: {
+  HelloWorld,
+  Form
+  },
+  })
+export default class Home extends Vue {}
+</script>
