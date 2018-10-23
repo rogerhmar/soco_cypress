32e96e5181519f29c4e6ec87bf6e6689f2c1e472
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2018-10-23 21:15:45 +0200

    gjør klar skjema-prosjekt

---
+++ cypress_smoketest_skjema/tests/fylle_ut_skjema.js
@@ -0,0 +1,16 @@
+
+describe('Skjema', () => {
+    before(() => {
+      cy.visit('/')
+    })
+    
+    it('Jeg forventer at det skal være mulig å fylle ut skjema', () => {
+      
+        // Fyll ut
+
+        // Trykk send
+
+        // Sjekk status
+
+    })
+})
